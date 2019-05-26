438542eb58abd0befbce7a1a3303e4d1fffa20d9
PedroAcA <pedro.almeidabsb@gmail.com>
2019-05-26 16:34:33 -0300

    Update GUI to accept x and y labels

--- GUI.py
+++ GUI.py
@@ -8,7 +8,7 @@ from matplotlib.figure import Figure
 import numpy as np
 import time
 class GUI:
-	def __init__(self, master=None):
+	def __init__(self, master=Tk()):
 		self.master = master
 		self.master.wm_title("ECG")
 		self.main_frame = Frame(self.master)
@@ -18,13 +18,15 @@ class GUI:
 		self.canvas = FigureCanvasTkAgg(plot_fig, master=self.master)
 		self.canvas.draw()
 		self.canvas.get_tk_widget().pack(side= TOP, fill= BOTH, expand=True)
-		quit_btn = Button(master=root, text="Quit", command=self._quit)
+		quit_btn = Button(master=self.master, text="Quit", command=self._quit)
 		quit_btn.pack(side = BOTTOM)
 		self.refresh()
 
-	def plot_signal(self, x, y):
+	def plot_signal(self, x, y, x_label='', y_label=''):
 		self.axis.clear()
 		self.axis.plot(x, y)
+		self.axis.set_xlabel(x_label)
+		self.axis.set_ylabel(y_label)
 		self.canvas.draw()
 		self.refresh()
 
@@ -38,15 +40,14 @@ class GUI:
 								# Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
 
-root = Tk()
-gui = GUI(root)
+gui = GUI()
 #Example to plot a signal and update GUI to show the plot
 t_start = 0
 t_end = 3
 while(True):
 	t = np.arange(t_start, t_end, .01)
-	base_sin = 20 * np.sin(2 * np.pi *10* t)
+	base_sine = 20 * np.sin(2 * np.pi *10* t)
 	random_noise = np.random.randn()* np.sin(2 * np.pi *np.random.randn()* t)
-	gui.plot_signal(t, np.add(base_sin, random_noise) )
+	gui.plot_signal(t, np.add(base_sine, random_noise), 'time', 'sine' )
 	t_start = t_end + 0.1
 	t_end = t_start + 3
